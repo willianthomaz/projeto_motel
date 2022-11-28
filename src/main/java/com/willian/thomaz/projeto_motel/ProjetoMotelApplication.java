3b7355df4e0ff0384ee07b963860ec217a555a2a
Willian Thomaz <willian_thomaz@outlook.com>
2022-11-28 17:37:37 -0300

    Initial commit

---
+++ src/main/java/com/willian/thomaz/projeto_motel/ProjetoMotelApplication.java
@@ -0,0 +1,13 @@
+package com.willian.thomaz.projeto_motel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMotelApplication.class, args);
+	}
+
+}
